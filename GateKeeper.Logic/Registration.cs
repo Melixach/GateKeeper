275898a70094f6b3c4b7905e5072f972aead562d
Kate Kerney <katekerney@gmail.com>
2018-02-17 21:48:35 -0800

    basic logic worked with @Melixach

---
+++ GateKeeper.Logic/Registration.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace GateKeeper.Logic
+{
+    public class Registration
+    {
+        public Registration()
+        {
+        }
+
+        const double AdultOvernightPrice = 50.00;
+        const double MemberOvernightPrice = 45.00;
+
+        public int AdultOvernightCount { get; private set; }
+        public double AdultOvernightTotalPrice { get; private set; }
+        public int MemberOvernightCount { get; private set; }
+        public double MemberOvernightTotalPrice { get; private set; }
+
+        public void AddAdultOvernight()
+        {
+            AdultOvernightCount++;
+            AdultOvernightTotalPrice = AdultOvernightPrice * AdultOvernightCount;
+        }
+
+        public void SubtractAdultOvernight()
+        {
+            if(AdultOvernightCount > 0)
+            {
+                AdultOvernightCount--;
+            }
+            else AdultOvernightCount = 0;
+            AdultOvernightTotalPrice = AdultOvernightPrice * AdultOvernightCount;
+        }
+
+        public void AddMemberOvernight()
+        {
+            MemberOvernightCount++;
+            MemberOvernightTotalPrice = MemberOvernightPrice * MemberOvernightCount;
+        }
+
+        public void SubtractMemberOvernight()
+        {
+            if (MemberOvernightCount > 0)
+            {
+                MemberOvernightCount--;
+            }
+            else MemberOvernightCount = 0;
+            MemberOvernightTotalPrice = MemberOvernightPrice * MemberOvernightCount;
+        }
+    }
+}
